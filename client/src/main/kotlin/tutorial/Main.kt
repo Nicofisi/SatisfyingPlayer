0e5e640e1e0093c584d7fd6880886649bef67149
Nicofisi <m.lomiak@gmail.com>
2018-08-01 19:42:23 +0200

    First broken commit

---
+++ client/src/main/kotlin/tutorial/Main.kt
@@ -0,0 +1,11 @@
+package tutorial
+
+import javax.swing.JFrame
+
+
+fun main(args: Array<String>) {
+    JFrame("SatisfyingPlayer")
+
+    PlayerFrame.openAndPlay(
+            "E:\\Documents\\Torrents\\The Hunger Games (2012) [1080p]\\The.Hunger.Games.1080p.BluRay.x264.YIFY.mp4")
+}
